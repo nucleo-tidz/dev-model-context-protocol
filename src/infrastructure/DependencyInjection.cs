4129a0fd4018fe36a2f83e4c2d5f23f4241fdc45
ahmar husain <nucleotidz@hotmail.com>
2025-05-17 12:01:52 +0530

    Commit

--- src/infrastructure/DependencyInjection.cs
+++ src/infrastructure/DependencyInjection.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using Microsoft.SemanticKernel;
 namespace infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection Add(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddSemanticKernel(this IServiceCollection services, IConfiguration configuration)
         {
-            return services;
+            return services.AddTransient<Kernel>(serviceProvider =>
+            {//AIzaSyB__RPh0X68ufHHPE9OLHnlSq4UDnz1z4c
+                IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+#pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+                kernelBuilder.Services.AddGoogleAIGeminiChatCompletion("gemini-2.0-flash", configuration["GeminiKey"], serviceId: "gpt-4-turbo");
+#pragma warning restore SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.       
+                return kernelBuilder.Build();
+            });
         }
 
     }
