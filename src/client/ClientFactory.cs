97acba317b950799e01d274587d47c0383142762
ahmar husain <nucleotidz@hotmail.com>
2025-05-18 14:19:56 +0530

    Fix

---
+++ src/client/ClientFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace client
+{
+    using System.Threading.Tasks;
+
+    using ModelContextProtocol.Client;
+
+    public class ClientFactory
+    {
+        public async Task<IMcpClient> Create()
+        {
+            var clientTransport = new StdioClientTransport(new StdioClientTransportOptions
+            {
+                Name = "nucleotidz-mcp-server",                
+                Command = "dotnet",
+                Arguments = ["run", "--project", "C:\\Workspace\\nucleo-tidz\\dev-model-context-protocol\\src\\server", "--no-build"],
+                
+            });
+            return await McpClientFactory.CreateAsync(clientTransport);
+        }
+    }
+}
---
+++ src/client/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using client;
+
+using infrastructure;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((config, services) =>
+    {
+        services.AddSemanticKernel(config.Configuration);
+        services.AddTransient<IBootStrapper, BootStrapper>();
+    }).Build();
+
+IBootStrapper aiservice = host.Services.GetRequiredService<IBootStrapper>();
+aiservice.Run().Wait();
