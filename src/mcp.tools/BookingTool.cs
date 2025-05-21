3b190e964c35cd209823d9139ae4e7a335715e5a
ahmar-husain <ahmar.husain@maersk.com>
2025-05-21 20:56:55 +0530

    Done

---
+++ src/mcp.tools/BookingTool.cs
@@ -0,0 +1,23 @@
+﻿using ModelContextProtocol.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mcp.tools
+{
+    [McpServerToolType]
+    public class BookingContainerTool
+    {
+        [McpServerTool, Description("Create booking from origin to destination for container")]
+        public static string ContainerState(string containerId,string from,string To)
+        {
+           
+            return "Booking Created";
+        }
+
+       
+    }
+}
