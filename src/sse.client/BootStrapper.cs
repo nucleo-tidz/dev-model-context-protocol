7c3cc369ba32987fd1923021cc501d44a0d4b740
ahmar-husain <ahmar.husain@maersk.com>
2025-05-21 20:35:58 +0530

    Fix

--- src/sse.client/BootStrapper.cs
+++ src/sse.client/BootStrapper.cs
@@ -7,33 +7,57 @@ using ModelContextProtocol.Client;
 
 namespace sse.client
 {
+    using System.Text;
+    using System.Threading.Tasks;
+
     using Microsoft.SemanticKernel;
+    using Microsoft.SemanticKernel.ChatCompletion;
     using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
 
     using ModelContextProtocol.Client;
 
-    public class BootStrapper(Kernel kernel) : IBootStrapper
+    public class BootStrapper : IBootStrapper
     {
-
+        Kernel _kernel;
+        private readonly IChatCompletionService _chatCompletionService;
+        public BootStrapper(Kernel kernel)
+        {
+            this._kernel = kernel;
+            this._chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+        }
         public async Task Run()
         {
             var clientfactory = new ClientFactory();
-            var client = await (clientfactory.Create());
-       
-            var tools = await client.ListToolsAsync();
+            var containerClient = await (clientfactory.CreateContainerClient());
+            var containerTools = await containerClient.ListToolsAsync();
 
+            _kernel.Plugins.AddFromFunctions("ShipmentContainerTool", containerTools.Select(_ => _.AsKernelFunction()));
 
-            kernel.Plugins.AddFromFunctions("ShipmentContainerTool", tools.Select(_ => _.AsKernelFunction()));
-                      var functionChoiceBehaviorOptions = new FunctionChoiceBehaviorOptions();
+            var vesselClient = await (clientfactory.CreateVesselClient());
+            var vesselTools = await vesselClient.ListToolsAsync();
 
-            functionChoiceBehaviorOptions.RetainArgumentTypes = true;           
-            AzureOpenAIPromptExecutionSettings azureOpenAIPromptExecutionSettings = new AzureOpenAIPromptExecutionSettings()
+            _kernel.Plugins.AddFromFunctions("VesselContainerTool", vesselTools.Select(_ => _.AsKernelFunction()));
+
+            ChatHistory chatHistory = new ChatHistory();
+            chatHistory.Add(new Microsoft.SemanticKernel.ChatMessageContent { Role = AuthorRole.System, Content = "You are a container shipment agent of a shipment comapny, your role is answer user query regarding conatainer shipment , vessel , and containers  ", });
+            Console.WriteLine("Ask Me");
+            while (true)
             {
-                Temperature = 0,
-                FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(options: functionChoiceBehaviorOptions),
-            };
+                string query = Console.ReadLine();
+                chatHistory.Add(new Microsoft.SemanticKernel.ChatMessageContent { Role = AuthorRole.User, Content = query });
 
-            var result = await kernel.InvokePromptAsync("What is the state of container with ID AHM1345 ?", new(azureOpenAIPromptExecutionSettings));
+
+                ChatMessageContent chatMessageContent = await _chatCompletionService.GetChatMessageContentAsync(chatHistory, new AzureOpenAIPromptExecutionSettings()
+                {
+                    Temperature = 0,
+                    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(options: new FunctionChoiceBehaviorOptions()
+                    {
+                        RetainArgumentTypes = true
+                    })
+                }, _kernel);
+                Console.WriteLine(chatMessageContent.Content);
+                chatHistory.Add(new Microsoft.SemanticKernel.ChatMessageContent { Role = AuthorRole.Assistant, Content = chatMessageContent.Content });
+            }
         }
     }
 
--- src/sse.client/ClientFactory.cs
+++ src/sse.client/ClientFactory.cs
@@ -4,12 +4,23 @@
     using ModelContextProtocol.Client;
     public class ClientFactory
     {
-        public async Task<IMcpClient> Create()
+        public async Task<IMcpClient> CreateContainerClient()
         {
             var clientTransport = new SseClientTransport(
                      new SseClientTransportOptions
                      {
                          Endpoint = new Uri("https://localhost:7196/sse")
+                     }
+                 );
+            return await McpClientFactory.CreateAsync(clientTransport);
+        }
+
+        public async Task<IMcpClient> CreateVesselClient()
+        {
+            var clientTransport = new SseClientTransport(
+                     new SseClientTransportOptions
+                     {
+                         Endpoint = new Uri("https://localhost:7289/sse")
                      }
                  );
             return await McpClientFactory.CreateAsync(clientTransport);
