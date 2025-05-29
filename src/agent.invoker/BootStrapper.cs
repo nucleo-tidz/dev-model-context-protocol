d3557e0f6487d26344df40d338ad4849fb1fc3b9
ahmar-husain <ahmar.husain@maersk.com>
2025-05-29 11:50:36 +0530

    Final

---
+++ src/agent.invoker/BootStrapper.cs
@@ -0,0 +1,58 @@
+﻿namespace client
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading.Tasks;
+
+    using shipment.agents.Group;
+
+    using Microsoft.SemanticKernel;
+    using Microsoft.SemanticKernel.ChatCompletion;
+
+    [Experimental("SKEXP0110")]
+    public class BootStrapper : IBootStrapper
+    {
+        private readonly Kernel _kernel;
+        public BootStrapper(Kernel kernel)
+        {
+            _kernel = kernel ?? throw new ArgumentNullException(nameof(kernel));
+        }
+
+#if false
+        public async Task Run()
+        {
+            var emailAgent = new EmailAgent(_kernel).Create();
+            ChatHistory chatHistory = new ChatHistory();
+            while (true)
+            {
+                string query = Console.ReadLine();
+                chatHistory.Add(new Microsoft.SemanticKernel.ChatMessageContent { Role = AuthorRole.User, Content = query });
+                await foreach (var message in emailAgent.InvokeAsync(chatHistory))
+                {
+                     
+                }
+            }
+        }
+#endif
+        public async Task Run()
+        {
+            GroupAgent groupAgent = new GroupAgent();
+            var agentGroupChat = groupAgent.CreateAgentGroupChat(_kernel);
+
+            while (true)
+            {
+                Console.WriteLine("give the command");
+                string query = Console.ReadLine();
+                agentGroupChat.AddChatMessage(new ChatMessageContent(AuthorRole.User, query));
+                await foreach (var content in agentGroupChat.InvokeAsync())
+                {
+                    if (!string.IsNullOrWhiteSpace(content.Content))
+                    {
+                        Console.WriteLine($"# {content.Role} - {content.AuthorName ?? "*"}: '{content.Content}'");
+                    }
+                    //Task.Delay(15000).Wait();
+                }
+            }
+        }
+    }
+}
---
+++ src/agents/shipment.agents/Group/GroupAgent.cs
@@ -0,0 +1,30 @@
+﻿namespace shipment.agents.Group
+{
+    using System.Diagnostics.CodeAnalysis;
+    using agents.Orchestrator;
+    using Microsoft.SemanticKernel;
+    using Microsoft.SemanticKernel.Agents;
+    using shipment.agents.Capacity;
+    using shipment.agents.Vessel;
+
+    [Experimental("SKEXP0110")]
+    public class GroupAgent()
+    {
+        public AgentGroupChat CreateAgentGroupChat(Kernel kernel)
+        {
+
+            var vesselAgent = new VesselAgent().Create(kernel);
+            var capacityAgent = new CapacityAgent().Create(kernel);
+            var bookingAgent = new BookingAgent().Create(kernel);
+
+
+            var chatOrchestrator = new Orchestrator(kernel);
+
+            return new AgentGroupChat(vesselAgent, capacityAgent, bookingAgent)
+            {
+                ExecutionSettings = chatOrchestrator.CreateExecutionSettings([vesselAgent,bookingAgent])
+
+            };
+        }
+    }
+}
