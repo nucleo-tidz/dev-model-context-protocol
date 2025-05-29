a66cc17cb0fd232167d05dd1756bcc9fce31a545
ahmar-husain <ahmar.husain@maersk.com>
2025-05-29 10:02:00 +0530

    fix

--- src/infrastructure/DependencyInjection.cs
+++ src/infrastructure/DependencyInjection.cs
@@ -9,7 +9,7 @@ namespace infrastructure
         public static IServiceCollection AddSemanticKernel(this IServiceCollection services, IConfiguration configuration)
         {
             return services.AddTransient<Kernel>(serviceProvider =>
-            {//https://www.linkedin.com/pulse/mcp-net-c-right-way-hosted-aspnetcore-aspire-what-else-latorre-zs3ff/
+            {
                 IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
                 kernelBuilder.Services.AddAzureOpenAIChatCompletion("gpt-4o",
                    "https://lighthouse-ai.openai.azure.com/",
