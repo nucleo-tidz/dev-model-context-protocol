a41d20eea2b1d05c77ef55f2ba40e37c1fb81ae5
ahmar-husain <ahmar.husain@maersk.com>
2025-06-02 10:55:23 +0530

    Remove extra space

--- src/servers/vessel.api/Program.cs
+++ src/servers/vessel.api/Program.cs
@@ -1,7 +1,6 @@
 
 using vessel.api;
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddMcpServer().WithHttpTransport().WithTools<VesselTool>();
 var app = builder.Build();
 app.UseHttpsRedirection();
